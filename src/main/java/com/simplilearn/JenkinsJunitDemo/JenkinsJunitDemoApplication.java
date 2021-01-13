db8d1bfaf86e027a91a2254df8bdda517f0a31c8
Siraj-ul-muneera <samreenmuneera@gmail.com>
2021-01-13 04:32:26 +0000

    Adding new files

---
+++ src/main/java/com/simplilearn/JenkinsJunitDemo/JenkinsJunitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JenkinsJunitDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsJunitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsJunitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/JenkinsJunitDemo/JenkinsJunitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JenkinsJunitDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsJunitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
